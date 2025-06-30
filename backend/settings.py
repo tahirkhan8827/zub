30a05ee5ea078533b7882f361b89e5351d8623a1
tahirkhan8827 <tahirkhan8827@gmail.com>
2025-06-30 10:51:21 +0530

    Render deployment setup

--- backend/settings.py
+++ backend/settings.py
@@ -151,8 +151,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+# settings.py
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
